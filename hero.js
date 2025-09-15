d2da1b31fdb51161e44f92ab9687f649f26c9522
kkyouus <andokarljohnreybautista@gmail.com>
2025-09-15 08:44:53 +0800

    Added New attack and Mana Left

--- hero.js
+++ hero.js
@@ -14,9 +14,27 @@ class Hero{
 
 class Warrior extends Hero{
     useAbility(){
-        console.log(`${this.name} uses power Strike`);
+        console.log(`${this.name} uses power Strike!!!`);
+        this.attack += 30;
+        console.log("New Attack: " + this.attack);
+    }
+}
+
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name} cast Fireball!!!`);
+        this.mana -= 5;
+        console.log("Mana Left: " + this.mana);
+
     }
 }
 
 const warrior = new Warrior("Thorin",100,10);
 warrior.useAbility();
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
