0e7f0317d2353204ad46cff2ee48fd70dac35c6f
kkyouus <andokarljohnreybautista@gmail.com>
2025-09-15 08:23:52 +0800

    Added warrior subclass and updated object instance

--- hero.js
+++ hero.js
@@ -12,7 +12,11 @@ class Hero{
     }
 }
 
-const hero = new Hero("Thorin",100,10);
+class Warrior extends Hero{
+    useAbility(){
+        console.log(`${this.name} uses power Strike`);
+    }
+}
 
-
-console.log(hero.getHealth());
+const warrior = new Warrior("Thorin",100,10);
+warrior.useAbility();
