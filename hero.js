2330891bb11ec457296951e8d367ecad136b80d2
kkyouus <andokarljohnreybautista@gmail.com>
2025-09-18 08:29:16 +0800

    Added addItem()

--- hero.js
+++ hero.js
@@ -5,10 +5,19 @@ class Hero{
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     }
 
     getHealth(){
         return this.#health;
+    }
+
+    getName(){
+        console.log(this.name);
+    }
+
+    getAttack(){
+        console.log(this.attack);
     }
 
     getStats(){
@@ -16,6 +25,14 @@ class Hero{
         console.log("Name: " + this.name);
         console.log("Health: " + this.#health);
         console.log("Attack: " + this.attack);
+    }
+
+    addItem(item){
+        this.items.push(item);
+    }
+
+    totalAttack(){
+        return this.attack + this.items.reduce((sum,i)=>sum+i.bonusAttack,0);
     }
 }
 
@@ -40,13 +57,33 @@ class Mage extends Hero{
     }
 }
 
-const Thorin = new Warrior("Thorin",100,10);
-Thorin.useAbility();
-Thorin.getStats();
+class Item{
+    constructor(name,bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
+}
 
-const Gandalf = new Mage("Gandalf",80,8,50);
-Gandalf.useAbility();
-Gandalf.getStats();
+const sword = new Item("Sword", 5);
+const staff = new Item("Staff", 3);
+
+const Thorin = new Warrior("Thorin",100,10);
+Thorin.getName();
+Thorin.getAttack();
+
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+Thorin.addItem(sword);
+
+//const Gandalf = new Mage("Gandalf",80,8,50);
+//Gandalf.useAbility();
+//Gandalf.getStats();
 
 function performAbility(hero){
     console.log("\n")
